04bb5b52083d6b78bd999c77af01e3cd0c4f95c7
IrkedCow <gavin.mc.collier@gmail.com>
2022-12-29 02:02:17 -0500

    added raido button for color selection
    this could be a selection box?

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import functools
-import makeGcode
+import makeInstuctions
 
 import tkinter as tk
 from tkinter import ttk
@@ -7,9 +6,10 @@ from tkinter import filedialog as fd
 
 # create the root window
 root = tk.Tk()
-root.title('Tkinter Open File Dialog')
+root.title('Pancake Gcode Generator')
 root.resizable(False, False)
 root.geometry('350x500')
+varColor = tk.IntVar()
 
 def select_file():
     filetypes = (('text files', '*.jpg'),)
@@ -18,7 +18,7 @@ def select_file():
         title='Open a .jpg',
         initialdir='/',
         filetypes=filetypes)    
-    makeGcode.generate_Gcode(filename)
+    makeInstuctions.makeInstuctions(filename, 152.4, 152.4, v.get())
 
 
 # open button
@@ -31,11 +31,33 @@ open_button = tk.Button(
     command=select_file
 )
 
+#the color picker
+v = tk.IntVar()
+v.set(0)  # initializing the choice, i.e. Python
+
+colors = [("Black", 0), ("White", 255), ("Gray", 127)]
+
+def ShowChoice():
+    print(v.get())
+
+for colors, val in colors:
+    tk.Radiobutton(root, 
+                   text=colors,
+                   padx = 20, 
+                   variable=v, 
+                   command=ShowChoice,
+                   value=val).grid(row = val + 2,sticky='W')
+
 # lable
 lable = tk.Label(root, text="Gavin's Great G-Code Generator!", font='Times 18')
 
-open_button.grid(column=0, row=5, padx=10, pady=30)
-lable.grid(column=0, row=0, padx=10, pady=10)
+open_button.grid(column=0, row=1, padx=10, pady=20)
+lable.grid(column=0, row=0, padx=10)
+
+#cBlack.grid(column=0, row=6, padx=40, sticky='W')
+#cWhite.grid(column=0, row=7, padx=40,sticky='W')
+#cGray.(column=0, row=8, padx=40,sticky='W')
+
 
 # run the application
 root.mainloop()
